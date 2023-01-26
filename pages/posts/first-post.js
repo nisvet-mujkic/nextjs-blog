9c13b5982d7b6680f7a7ee4ea6f90805f57e359d
Nisvet Mujkić <nisvet.mujkic@outlook.com>
2023-01-26 21:58:32 +0100

    css stuff. not a fan

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,25 @@
+import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post</h1>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() => {
+            console.log("YEY!");
+          }}
+        />
+      </Head>
+      <h1 className="">First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
