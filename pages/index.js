78d16c4d9f28b0c0c3acc5a12429fec2ba9ea2e7
Nisvet Mujkić <nisvet.mujkic@outlook.com>
2023-01-27 10:11:04 +0100

    add blog posts

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,19 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import styles from "../styles/Home.module.css";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const posts = getSortedPostsData();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function Home({ posts }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +24,20 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Nisvet. Welcome to my digital space. :)</p>
         <p>This is me learning how to make a blog</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {posts.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
